f6bf82e6a64da01d83fc3cb0cdcbd75614c245ef
EmelieBengtsson <idacarolineemelie@gmail.com>
2022-12-04 20:32:50 +0100

    ..

--- src/main/java/Day1.java
+++ src/main/java/Day1.java
@@ -1,4 +1,37 @@
 package main.java;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Day1 {
+    public static void main (String[] args){
+        int elfCounter = 0;
+        int elfWithMostCalories = 0;
+        int sumHighest =0;
+        boolean endOfFile = false;
+
+        String filename = "day1.txt";
+
+        try(Scanner sc = new Scanner(new File(filename));
+
+
+
+        while(!endOfFile){
+            int sum = 0;
+            while (sc.hasNextInt()){
+                sum+=sc.nextInt();
+            }
+            if (sum > sumHighest){
+                elfWithMostCalories = elfCounter;
+            }
+            elfCounter++;
+
+            if(!sc.hasNextLine()){
+                endOfFile = true;
+            }
+        }
+
+        System.out.println("The Elf with most calories is " + elfWithMostCalories);
+    }
+
 }
